59e234bc361bb0d505894ce771d857815c1c63e1
Manuel Gruber <m@manuelgruber.com>
2015-11-01 11:42:22 +0400

    Moved to css injection

--- source/clean.js
+++
@@ -1,7 +0,0 @@
-function onLoad(){
-  var element = document.querySelector('#board > div.js-add-list.list-wrapper.mod-add.is-idle');
-  element.parentNode.removeChild(element);
-}
-
-document.addEventListener('DOMContentLoaded', onLoad);
-
